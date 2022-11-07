d76869120ca61117a19e18feb503e2df4819ec44
binaykushwaha <14cse190giet@gmail.com>
2022-11-07 19:07:26 +0545

    update for login

--- Goover/ClientApp/src/app/msteams/personal-tabs/home/home.component.ts
+++ Goover/ClientApp/src/app/msteams/personal-tabs/home/home.component.ts
@@ -56,43 +56,63 @@ export class HomeComponent implements OnInit {
         console.error("Error occured while calling API: postRequestVerificationByEmail", error);
       });
   }
-  
-  verifySmsCode(countryCode: string, phoneNumber: string, login: string, code: string) {
-    this.homeService.verifySmsCode(countryCode, phoneNumber, login, code).subscribe(result => {
-      if (result) {
-        this.isLoginEnabled = true;
-      }
-    },
-      error => {
-        console.error("Error occured while calling API: postRequestVerificationByEmail", error);
+
+  loginSubmit() {
+    if (this.active == 1) {
+
+      //verify sms code
+      this.homeService.verifySmsCode(this.selectedCountryCode, this.PhoneNumber, this.PhoneNumber, this.SmsCode).subscribe(result => {
+        if (result) {
+          //enable login input fields
+          this.isLoginEnabled = true;
+
+          console.log("sms code successful");
+
+          //authenticate via phone number
+          this.login(this.PhoneNumber, this.SmsCode, this.smsRememberMeChecked);
+        }
+      },
+        error => {
+          console.error("Error occured while calling API: verifySmsCode", error);
+        });
+    }
+    else if (this.active == 2) {
+
+      // verify email code
+      this.homeService.verifyEmailCode(this.email, this.email, this.emailCode).subscribe(result => {
+        if (result) {
+
+          console.log("email code successful");
+
+          //enable login input fields
+          this.isLoginEnabled = true;
+
+          //authenticate via email
+          this.login(this.email, this.emailCode, this.emailRememberMeChecked)
+        }
+      }, error => {
+        console.error("Error occured while calling API: verifyEmailCode", error);
       });
-  }
-
-  verifyEmailCode(email: string, login: string, code: string) {
-    this.homeService.verifyEmailCode(email, login, code).subscribe(result => {
-      if (result) {
-        this.isLoginEnabled = true;
-      }
-    }, error => {
-      console.error("Error occured while calling API: postRequestVerificationByEmail", error);
-    });
-  }
-
-  onLogin() {
-    if (this.isEmailTabSelected) {
-
     }
   }
 
   login(username: string, password: string, rememberMe: boolean) {
 
+    console.log("authentication started.")
+
     this.homeService.authenticate(username, password, rememberMe, this.deviceId).subscribe(res => {
       if (res != null && res.secret_key != null) {
         this.secretKey = res.secret_key;
 
+        console.log("authentication completed.")
+        console.log("Successfully got secret key");
+
         this.homeService.getAuthenticationToken(res.secret_key).subscribe(res => {
           if (res != null && res.secret_key != null) {
             this.accessToken = res.id_token;
+
+            console.log("Login successful");
+            console.log("access token", this.accessToken);
           }
         }, error => {
           console.error("Error occured while calling API: GetToken", error);
